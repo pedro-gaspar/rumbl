26212b451a5a75f01aa4fa9d9192e27d47116824
Pedro Gaspar <pedro.mig.gaspar@gmail.com>
2021-07-19 12:35:00 +0100

    Ecto

--- lib/rumbl_web/router.ex
+++ lib/rumbl_web/router.ex
@@ -16,6 +16,8 @@ defmodule RumblWeb.Router do
   scope "/", RumblWeb do
     pipe_through :browser
 
+    resources "/users", UserController, only: [:index, :show, :new, :create]
+
     get "/users", UserController, :index
     get "/users/:id", UserController, :show
     get "/", PageController, :index
